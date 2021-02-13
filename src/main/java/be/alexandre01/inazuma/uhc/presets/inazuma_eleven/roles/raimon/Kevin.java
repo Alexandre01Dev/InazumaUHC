1461c75550b1d4f06f3c7c129ed16f95a8812461
Nan0uche <78554163+Nan0uche@users.noreply.github.com>
2021-02-13 14:10:09 +0100

    Update Roles avec tout les roles dans InazumaEleven afin de te forcer a faire un menu de config des roles

--- src/main/java/be/alexandre01/inazuma/uhc/presets/inazuma_eleven/roles/raimon/Kevin.java
+++ src/main/java/be/alexandre01/inazuma/uhc/presets/inazuma_eleven/roles/raimon/Kevin.java
@@ -1,9 +1,36 @@
 package be.alexandre01.inazuma.uhc.presets.inazuma_eleven.roles.raimon;
 
+import be.alexandre01.inazuma.uhc.presets.Preset;
+import be.alexandre01.inazuma.uhc.presets.inazuma_eleven.categories.Alius;
 import be.alexandre01.inazuma.uhc.roles.Role;
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class Kevin extends Role {
     public Kevin() {
         super("Kevin Dragonfly");
+
+        addCommand("intimidate", new command() {
+            public int i = 0;
+            @Override
+            public void a(CommandSender sender, Command cmd, String label, String[] args) {
+                Player player = (Player) sender;
+                Player target = Bukkit.getPlayer(args[0]);
+                if(i > 3){
+                    player.sendMessage(Preset.instance.p.prefixName()+" Vous avez dépassé le nombre d'utilisation de cette commande");
+                    return;
+                }
+                target.addPotionEffect(new PotionEffect(PotionEffectType.WEAKNESS, 30, 1,false,false), true);
+                target.addPotionEffect(new PotionEffect(PotionEffectType.SLOW, 10, 1,false,false), true);
+                player.sendMessage(Preset.instance.p.prefixName()+"Vous avez intimidé"+target+".");
+                player.sendMessage(Preset.instance.p.prefixName()+"Kevin vous a intimidé");
+                i++;
+            }
+        });
     }
 }
