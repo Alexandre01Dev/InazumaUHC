621641c3a4f05c54aa8b98c23d0066cbadd02649
Alexandre01Dev <altata01@hotmail.com>
2021-02-17 14:31:39 +0100

    Role: Patch Jude [1]

--- src/main/java/be/alexandre01/inazuma/uhc/presets/inazuma_eleven/roles/raimon/Jude.java
+++ src/main/java/be/alexandre01/inazuma/uhc/presets/inazuma_eleven/roles/raimon/Jude.java
@@ -12,6 +12,8 @@ import org.bukkit.craftbukkit.v1_8_R3.scoreboard.CraftScoreboard;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntityRegainHealthEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -19,31 +21,34 @@ import org.bukkit.scoreboard.*;
 import org.bukkit.scoreboard.Scoreboard;
 
 public class Jude extends Role implements Listener {
+
+    private Scoreboard score = null;
     public Jude() {
         super("Jude Sharp");
         setRoleCategory(Raimon.class);
+        addListener(this);
         onLoad(new load() {
             @Override
             public void a() {
                 Bukkit.getScheduler().scheduleSyncDelayedTask(InazumaUHC.get, new Runnable() {
                     @Override
                 public void run() {
-                        Scoreboard score = ScoreboardUtil.get.clone(Bukkit.getScoreboardManager().getMainScoreboard(),true);
+                        score = ScoreboardUtil.get.clone(Bukkit.getScoreboardManager().getMainScoreboard(),true);
 
                         if(score.getObjective("showhealth")==null){
-                            Objective o = score.registerNewObjective("showhealth","health");
+                            Objective o = score.registerNewObjective("showhealth","dummy");
                             System.out.println("SHOWHEALTH SETUP");
-                            o.setDisplayName("§c❤");
+                            o.setDisplayName("%§c❤");
                             o.setDisplaySlot(DisplaySlot.BELOW_NAME);
                         }
                   for(Player player : getPlayers()){
                     player.addPotionEffect(new PotionEffect(PotionEffectType.SPEED, Integer.MAX_VALUE, 0,false,false), true);
                       player.setScoreboard(score);
+                      setHealth(player);
                     for(Player opposants : Bukkit.getOnlinePlayers()){
                        // new ScoreboardScore(s, o, "§c❤").setScore(2);
+                        setHealth(opposants);
 
-                        player.setHealth(player.getHealth());
-                        opposants.setHealth(opposants.getHealth());
                     }
                   }
 
@@ -55,5 +60,28 @@ public class Jude extends Role implements Listener {
                 }
 
         });
+
+
+    }
+    private void setHealth(Player player){
+        int pourcentage  = (int) ((100*player.getHealth()) /player.getMaxHealth());
+        Score s = score.getObjective("showhealth").getScore(player);
+        s.setScore(pourcentage);
+    }
+
+    @EventHandler
+    public void onDamage(EntityDamageEvent event){
+        if(event.getEntity() instanceof Player){
+            Player player = (Player) event.getEntity();
+            setHealth(player);
+        }
+    }
+
+    @EventHandler
+    public void onHeart(EntityRegainHealthEvent event){
+        if(event.getEntity() instanceof Player){
+            Player player = (Player) event.getEntity();
+            setHealth(player);
+        }
     }
 }
