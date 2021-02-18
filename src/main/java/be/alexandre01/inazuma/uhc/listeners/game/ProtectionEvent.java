f9b620ce6cf94d1b2c008eb0326f776c2b30a451
Alexandre01Dev <altata01@hotmail.com>
2021-02-18 02:28:10 +0100

    Patch Door on Spawn and ArmorStand bug

--- src/main/java/be/alexandre01/inazuma/uhc/listeners/game/ProtectionEvent.java
+++ src/main/java/be/alexandre01/inazuma/uhc/listeners/game/ProtectionEvent.java
@@ -1,5 +1,7 @@
 package be.alexandre01.inazuma.uhc.listeners.game;
 
+import org.bukkit.Material;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
@@ -8,7 +10,10 @@ import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.block.BlockPlaceEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntitySpawnEvent;
+import org.bukkit.event.player.PlayerInteractAtEntityEvent;
+import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class ProtectionEvent implements Listener {
     @EventHandler
@@ -22,7 +27,9 @@ public class ProtectionEvent implements Listener {
 
     @EventHandler
     public void onEntitySpawn(EntitySpawnEvent event){
-        event.setCancelled(true);
+        if(!(event.getEntity() instanceof ArmorStand)){
+            event.setCancelled(true);
+        }
     }
 
     @EventHandler
@@ -37,6 +44,51 @@ public class ProtectionEvent implements Listener {
     }
     @EventHandler
     public void onInteract(PlayerInteractEvent event){
+        if(event.getClickedBlock() != null){
+            Material m = event.getClickedBlock().getType();
+            if(m.equals(Material.ACACIA_DOOR)){
+                return;
+            }
+
+            if(m.equals(Material.DARK_OAK_DOOR)){
+                return;
+            }
+
+            if(m.equals(Material.BIRCH_DOOR)){
+                return;
+            }
+
+            if(m.equals(Material.DARK_OAK_DOOR_ITEM)){
+                return;
+            }
+
+            if(m.equals(Material.JUNGLE_DOOR)){
+                return;
+            }
+
+            if(m.equals(Material.SPRUCE_DOOR)){
+                return;
+            }
+
+            if(m.equals(Material.WOOD_DOOR)){
+                return;
+            }
+
+            if(m.equals(Material.WOODEN_DOOR)){
+                return;
+            }
+
+        }
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onInteractAtEntity(PlayerInteractAtEntityEvent event){
+        event.setCancelled(true);
+    }
+
+    @EventHandler
+    public void onInteractEntity(PlayerInteractEntityEvent event){
         event.setCancelled(true);
     }
 }
