5527b102bd692de3261f59d6647fde500cf5da2f
Alexandre01Dev <altata01@hotmail.com>
2021-02-19 14:05:45 +0100

    Team FIX

--- src/main/java/be/alexandre01/inazuma/uhc/spectators/BukkitTeamInitializer.java
+++ src/main/java/be/alexandre01/inazuma/uhc/spectators/BukkitTeamInitializer.java
@@ -1,10 +1,27 @@
 package be.alexandre01.inazuma.uhc.spectators;
 
+import be.alexandre01.inazuma.uhc.presets.Preset;
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.scoreboard.NameTagVisibility;
 import org.bukkit.scoreboard.Scoreboard;
 import org.bukkit.scoreboard.Team;
 
 public class BukkitTeamInitializer {
+    public static Team setAlive(Player player){
+        Scoreboard score = Bukkit.getScoreboardManager().getMainScoreboard();
+
+        if(score.getTeam(player.getName())==null){
+            score.registerNewTeam(player.getName());
+        }
+        Team tV = score.getTeam(player.getName());
+        tV.setPrefix("§7");
+        tV.setCanSeeFriendlyInvisibles(false);
+        tV.setNameTagVisibility(NameTagVisibility.ALWAYS);
+        tV.setDisplayName(player.getName());
+        tV.addPlayer(player);
+        return tV;
+    }
     public static void initialize(){
 
         Scoreboard score = Bukkit.getScoreboardManager().getMainScoreboard();
@@ -15,7 +32,7 @@ public class BukkitTeamInitializer {
         Team tV = score.getTeam("0VIVANT");
         tV.setPrefix("§7");
         tV.setCanSeeFriendlyInvisibles(false);
-
+        tV.setNameTagVisibility(NameTagVisibility.ALWAYS);
 
         if(score.getTeam("999MORT")==null){
             score.registerNewTeam("999MORT");
