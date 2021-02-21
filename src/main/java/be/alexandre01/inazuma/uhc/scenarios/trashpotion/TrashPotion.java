f5c279e0dd5c73aee2656b4599837d5f7276c00e
Nan0uche <78554163+Nan0uche@users.noreply.github.com>
2021-02-21 23:55:46 +0100

    TrashPotion FINAL 2.0.1.2

--- src/main/java/be/alexandre01/inazuma/uhc/scenarios/trashpotion/TrashPotion.java
+++ src/main/java/be/alexandre01/inazuma/uhc/scenarios/trashpotion/TrashPotion.java
@@ -1,60 +1,33 @@
 package be.alexandre01.inazuma.uhc.scenarios.trashpotion;
 
-import be.alexandre01.inazuma.uhc.InazumaUHC;
 import be.alexandre01.inazuma.uhc.scenarios.Scenario;
 import be.alexandre01.inazuma.uhc.utils.ItemBuilder;
 import org.bukkit.Bukkit;
 import org.bukkit.Material;
-import org.bukkit.entity.Item;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerItemConsumeEvent;
-import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
-import org.bukkit.potion.Potion;
-import org.bukkit.potion.PotionEffect;
-import org.bukkit.scheduler.BukkitRunnable;
+import org.bukkit.plugin.Plugin;
 
 public class TrashPotion extends Scenario implements Listener {
     public TrashPotion() {
-        super("TrashPotion", "Clear la potion une fois bu.");
+        super("TrashPotion", "Clear une potion une fois bu.");
         addListener(this);
         ItemBuilder itemBuilder = new ItemBuilder(Material.GLASS_BOTTLE);
         setItemStack(itemBuilder.toItemStack());
     }
     @EventHandler
-    public void onDrinkPotion(PlayerItemConsumeEvent e){
-            if (e.getItem().getType().equals(Material.POTION)){
-                Player player = e.getPlayer();
-                ItemStack p = e.getItem();
-                System.out.println("Potion bu");
-                Potion potion = Potion.fromItemStack(e.getItem());
+    public void onConsume(PlayerItemConsumeEvent e) {
+        final Player player = e.getPlayer();
 
-                Bukkit.getScheduler().runTaskLater(InazumaUHC.get, new BukkitRunnable() {
-                    @Override
-                    public void run() {
-                        for(ItemStack it : player.getInventory().getContents()){
-                            if(it != null){
-                                System.out.println(it.getType());
-                                System.out.println(p.getType());
-                                if(it.equals(p)){
-                                    System.out.println("TROUVE BORDEL");
-                                    for(PotionEffect potionEffect : potion.getEffects()){
-                                        player.addPotionEffect(potionEffect);
-                                    }
-                                    it.setType(Material.AIR);
-                                    player.updateInventory();
-                                    break;
-                                }
-                            }
-                        }
-                    }
-                },10);
-
-
-
-            }
+        if (e.getItem().getTypeId() == 373) {
+            Bukkit.getServer().getScheduler().runTaskLaterAsynchronously((Plugin) this, new Runnable() {
+                public void run() {
+                    player.setItemInHand(new ItemStack(Material.AIR));
+                }
+            }, 1L);
+        }
     }
-
 }
