7e34f58f14b14a17194c07d6778bcee9ed20166a
Alexandre01Dev <altata01@hotmail.com>
2021-02-21 18:11:58 +0100

    Add description to Role [2]

--- src/main/java/be/alexandre01/inazuma/uhc/roles/Role.java
+++ src/main/java/be/alexandre01/inazuma/uhc/roles/Role.java
@@ -203,7 +203,9 @@ public class Role {
     public ArrayList<String> getDescription() {
         return description;
     }
+    public void addDescription(){
 
+    }
     public void setDescription(int line,String description) {
        this.description.add(line,description);
     }
