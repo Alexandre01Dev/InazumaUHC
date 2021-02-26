ea2424b7d23138a10e5c0c55c5db2906fa1174fa
Nan0uche <78554163+Nan0uche@users.noreply.github.com>
2021-02-26 18:47:33 +0100

    Scoreboard pour voir joueur en vie et pour ALex implement un timer de 20 min qui se reset?

--- src/main/java/be/alexandre01/inazuma/uhc/presets/inazuma_eleven/scoreboards/GameScoreboard.java
+++ src/main/java/be/alexandre01/inazuma/uhc/presets/inazuma_eleven/scoreboards/GameScoreboard.java
@@ -42,23 +42,24 @@ public class GameScoreboard {
                     objectiveSign.setDisplayName("§8»§3§lInazuma§8§l-§3§lEleven§8«");
 
                     objectiveSign.setLine(4, "§r§l§8»§8§m------------§l§8«");
-                    objectiveSign.setLine(5,"§7Episode §l» §e"+ Episode.getEpisode());
-                    objectiveSign.setLine(6, "§7Temps §l» §e" + Bukkit.getOnlinePlayers().size() + "§7/§e"+inazuma.getPlayerSize());
-                    objectiveSign.setLine(7, "§r§l§8»§8§m------------§l§8«§f");
-                    objectiveSign.setLine(8, "§7"+ InazumaEleven.timerText+" §l» §e" + InazumaEleven.timerValue);
+                    objectiveSign.setLine(5, "§7Joueurs §l» §e" + Bukkit.getOnlinePlayers().size());
+                    objectiveSign.setLine(6,"§7Episode §l» §e"+ Episode.getEpisode());
+                    objectiveSign.setLine(7, "§7Temps §l» §e" + "A dev");
+                    objectiveSign.setLine(8, "§r§l§8»§8§m------------§l§8«§f");
+                    objectiveSign.setLine(9, "§7"+ InazumaEleven.timerText+" §l» §e" + InazumaEleven.timerValue);
                     if(inazuma.hasNether()){
-                        objectiveSign.setLine(9,"§7"+InazumaEleven.netherText+" §l» §e" + InazumaEleven.netherValue);
+                        objectiveSign.setLine(10,"§7"+InazumaEleven.netherText+" §l» §e" + InazumaEleven.netherValue);
                     }
-                    objectiveSign.setLine(10,"§7"+InazumaEleven.bordureText+" §l» §e" + InazumaEleven.bordureValue);
-                    objectiveSign.setLine(16, "§r§l§8»§8§m------------§l§8«§r");
+                    objectiveSign.setLine(11,"§7"+InazumaEleven.bordureText+" §l» §e" + InazumaEleven.bordureValue);
+                    objectiveSign.setLine(17, "§r§l§8»§8§m------------§l§8«§r");
                     World world = player.getWorld();
                     int borderSize = (int) world.getWorldBorder().getSize() /2;
-                    objectiveSign.setLine(17,"§7Bordure §l» §e-"+ borderSize+"§7/§e"+borderSize);
+                    objectiveSign.setLine(18,"§7Bordure §l» §e-"+ borderSize+"§7/§e"+borderSize);
 
-                    objectiveSign.setLine(18,"§7Centre §l» §e "+ inazuma.getArrows().get(player.getUniqueId()));
-                    objectiveSign.setLine(19, "§l§8»§8§m------------§l§8«");
+                    objectiveSign.setLine(19,"§7Centre §l» §e "+ inazuma.getArrows().get(player.getUniqueId()));
+                    objectiveSign.setLine(20, "§l§8»§8§m------------§l§8«");
 
-                    objectiveSign.setLine(20, "§7Scénario(s) §l» "+scenario);
+                    objectiveSign.setLine(21, "§7Scénario(s) §l» "+scenario);
 
 
                     objectiveSign.setLine(21, "§l§8»§8§m------------§l§8« ");
