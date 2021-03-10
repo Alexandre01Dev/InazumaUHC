8a0f281a6af7ae83929ed98b866e4b090b0221d0
Alexandre01Dev <altata01@hotmail.com>
2021-03-11 00:06:29 +0100

    Patch David

--- src/main/java/be/alexandre01/inazuma/uhc/presets/inazuma_eleven/roles/alius/David.java
+++ src/main/java/be/alexandre01/inazuma/uhc/presets/inazuma_eleven/roles/alius/David.java
@@ -66,6 +66,9 @@ public class David extends Role {
                         }
                     });
 
+                    giveItem(player);
+
+                    player.sendMessage(Preset.instance.p.prefixName()+" Tu viens de recevoir §bManchot Empereur N°1 dans ton inventaire");
                     return;
                 }
                 if (args[0].equalsIgnoreCase("refuse")) {
