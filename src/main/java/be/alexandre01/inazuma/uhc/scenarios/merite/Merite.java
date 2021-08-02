3d59ab7dd2d8f9d6bffa5a8d4c6b844b7930ddab
Erio6 <eliotgruscha@gmail.com>
2021-08-02 23:35:28 +0200

    Merite fin

--- src/main/java/be/alexandre01/inazuma/uhc/scenarios/merite/Merite.java
+++ src/main/java/be/alexandre01/inazuma/uhc/scenarios/merite/Merite.java
@@ -5,6 +5,7 @@ import be.alexandre01.inazuma.uhc.custom_events.player.PlayerInstantDeathEvent;
 import be.alexandre01.inazuma.uhc.presets.Preset;
 import be.alexandre01.inazuma.uhc.scenarios.Scenario;
 import be.alexandre01.inazuma.uhc.utils.ItemBuilder;
+import lombok.Getter;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
@@ -13,6 +14,7 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.inventory.CraftItemEvent;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
@@ -58,6 +60,66 @@ public class Merite extends Scenario implements Listener {
 
     }
 
+    public float getSwordPourcent(Player player)
+    {
+        if(swordPourcent.containsKey(player))
+        {
+            return swordPourcent.get(player);
+        }
+        return 0;
+    }
+    public float getArmorPourcent(Player player)
+    {
+        if(armorPourcent.containsKey(player))
+        {
+            return armorPourcent.get(player);
+        }
+        return 0;
+    }
+    public float getBowPourcent(Player player)
+    {
+        if(bowPourcent.containsKey(player))
+        {
+            return bowPourcent.get(player);
+        }
+        return 0;
+    }
+    public float getSword(Player player)
+    {
+        if(swordMap.containsKey(player))
+        {
+            return swordMap.get(player);
+        }
+        return 0;
+    }
+    public float getArmor(Player player)
+    {
+        if(armorMap.containsKey(player))
+        {
+            return armorMap.get(player);
+        }
+        return 0;
+    }
+    public float getBow(Player player)
+    {
+        if(bowMap.containsKey(player))
+        {
+            return bowMap.get(player);
+        }
+        return 0;
+    }
+
+    public float getKill(Player player)
+    {
+        if(points.containsKey(player))
+        {
+            return points.get(player);
+        }
+        return 0;
+    }
+
+
+
     @EventHandler
     public void onDamage(EntityDamageByEntityEvent event){
 
@@ -82,6 +144,48 @@ public class Merite extends Scenario implements Listener {
         }
     }
 
+    @EventHandler
+    void onClick(InventoryClickEvent event)
+    {
+        ItemStack current = event.getCurrentItem();
+        Player player = (Player) event.getWhoClicked();
+        if(current == null)
+            return;
+
+        if(event.getClickedInventory().getName().equalsIgnoreCase("Boostez votre multiplicateur"))
+        {
+            switch (current.getItemMeta().getDisplayName())
+            {
+                case "§6Mêlée" : if(swordPourcent.containsKey(player))
+                {
+                    if(points.containsKey(player) && points.get(player) != 0)
+                    {
+                        swordPourcent.replace(player, swordPourcent.get(player) + 0.05f);
+                        points.replace(player, points.get(player) - 1);
+                    }
+
+                }
+                break;
+
+                case "§6Armure" : if(armorPourcent.containsKey(player))
+                {
+                    armorPourcent.replace(player, armorPourcent.get(player) + 0.05f);
+                    points.replace(player, points.get(player) - 1);
+                }
+                break;
+
+                case "§6Arc" : if(bowPourcent.containsKey(player))
+                {
+                    bowPourcent.replace(player, bowPourcent.get(player) + 0.05f);
+                    points.replace(player, points.get(player) - 1);
+                }
+                break;
+
+                default: break;
+            }
+        }
+    }
+
     void addSword(Player player, float i)
     {
         if(swordMap.containsKey(player))
@@ -91,7 +195,7 @@ public class Merite extends Scenario implements Listener {
             {
                 multiplicator = swordPourcent.get(player);
             }
-            swordMap.put(player, swordMap.get(player) + (((int)(i*multiplicator*100))/100));
+            swordMap.replace(player, swordMap.get(player) + (((int)(i*multiplicator*100))/100));
             checkSword(player, player.getItemInHand());
 
         }
@@ -109,7 +213,7 @@ public class Merite extends Scenario implements Listener {
             {
                 multiplicator = armorPourcent.get(player);
             }
-            armorMap.put(player, armorMap.get(player) + (((int)(i*multiplicator*100))/100));
+            armorMap.replace(player, armorMap.get(player) + (((int)(i*multiplicator*100))/100));
             checkArmor(player, player.getInventory().getArmorContents());
 
         }
@@ -126,7 +230,7 @@ public class Merite extends Scenario implements Listener {
             {
                 multiplicator = bowPourcent.get(player);
             }
-            bowMap.put(player, bowMap.get(player) + (((int)(i*multiplicator*100))/100));
+            bowMap.replace(player, bowMap.get(player) + (((int)(i*multiplicator*100))/100));
             checkBow(player, player.getItemInHand());
 
         }
@@ -307,7 +411,7 @@ public class Merite extends Scenario implements Listener {
         Player killer = event.getKiller();
         if(points.containsKey(killer))
         {
-            points.put(killer, points.get(killer) + 1);
+            points.replace(killer, points.get(killer) + 1);
         }
     }
 }
--- src/main/java/be/alexandre01/inazuma/uhc/scenarios/merite/MeriteBoostCommand.java
+++ src/main/java/be/alexandre01/inazuma/uhc/scenarios/merite/MeriteBoostCommand.java
@@ -1,8 +1,18 @@
 package be.alexandre01.inazuma.uhc.scenarios.merite;
 
+import be.alexandre01.inazuma.uhc.utils.ItemBuilder;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.SkullType;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.SkullMeta;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MeriteBoostCommand extends Command {
 
@@ -14,13 +24,20 @@ public class MeriteBoostCommand extends Command {
     }
 
     @Override
-    public boolean execute(CommandSender commandSender, String label, String[] args) {
-        if(commandSender instanceof Player)
+    public boolean execute(CommandSender sender, String label, String[] args) {
+        if(sender instanceof Player)
         {
-            if(merite.players.contains(commandSender))
+            Player player = (Player) sender;
+            if(merite.players.contains(sender))
             {
-                //Faire l'inv pour incrémenter le multiplicateur et gerer le systeme de points pour l'incrémentation
-                //GL HF
+                Inventory inv = Bukkit.createInventory(null, 54, "Boostez votre multiplicateur");
+
+                inv.setItem(20, new ItemBuilder(Material.IRON_SWORD).setName("§6Mêlée").setLore("Multiplicateur : " + merite.getSwordPourcent(player)+ " + 0.05 §7(Prix » 1 kill)").toItemStack() );
+                inv.setItem(22, new ItemBuilder(Material.IRON_CHESTPLATE).setName("§6Armure").setLore("Multiplicateur : " + merite.getArmorPourcent(player)+ " + 0.05 §7(Prix » 1 kill)").toItemStack());
+                inv.setItem(24, new ItemBuilder(Material.BOW).setName("§6Arc").setLore("Multiplicateur : " + merite.getBowPourcent(player)+ " + 0.05 §7(Prix » 1 kill)").toItemStack());
+                inv.setItem(49, new ItemBuilder(Material.SKULL_ITEM).setSkullOwner(sender.getName()).setName("§6Stats").setLore("Mêlée » §7 " + merite.getSword(player) + "\n§rArmure  » §7 " + merite.getArmor(player) + "\n§rArc » §7" + merite.getBow(player) + "\n§rPoint(s) disponible(s) » §7" + merite.getKill(player)).toItemStack());
+                player.openInventory(inv);
+
             }
         }
         return false;
